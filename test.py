c42fd4854e6200c59288b030ff40b13c36dd155f
Kirilin Artem <kirilinartem@yandex.ru>
2025-03-16 00:36:50 +0700

    Вычисление необходимых изменений в лотах

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ from tinkoff.invest import Client, InstrumentIdType, Quotation, HistoricCandle
 import os
 import datetime as dt 
 from icecream import ic
+import pandas as pd
+
+def get_target_rate() -> pd.Series:
+    return pd.Series([
+        0.5
+        ,0.5
+    ])
 
 def day_start(day: dt.datetime):
     return dt.datetime(day.year, day.month, day.day, tzinfo=day.tzinfo)
@@ -9,10 +16,14 @@ def day_start(day: dt.datetime):
 def day_end(day: dt.datetime):
     return dt.datetime(day.year, day.month, day.day, tzinfo=day.tzinfo) + dt.timedelta(days=1)# - dt.timedelta(seconds=1)
 
+def quotation_to_float(q: Quotation):
+    return float(q.units) + float(q.nano/10**9)
+
 token = os.getenv('TINKOFF_API_TOKEN')
 account_id = '2224375246'
 with Client(token) as client:
     # ic(client.users.get_accounts())
+    all_positions = []
     for position in client.operations.get_portfolio(account_id=account_id).positions:
         instrument_info = client.instruments.get_instrument_by(id_type=1,id=position.figi).instrument
         anchor_day = dt.datetime.now(dt.timezone.utc) - dt.timedelta(1)
@@ -35,16 +46,39 @@ with Client(token) as client:
         else:
             candle = candle[0]
         # ic(candle)
-        deep_position = {
+        positions_with_info = {
             'figi': position.figi
+            ,'ticker':instrument_info.ticker
             ,'instrument_type': position.instrument_type
-            ,'quantity': position.quantity
-            ,'quantity_lot': position.quantity_lots
+            ,'quantity': quotation_to_float(position.quantity)
+            ,'quantity_lot': quotation_to_float(position.quantity_lots)
             ,'lot': instrument_info.lot
             ,'currency': instrument_info.currency
             ,'name': instrument_info.name
             ,'class_code': instrument_info.class_code
             ,'exchange': instrument_info.exchange
-            ,'price': candle.close
+            ,'price': quotation_to_float(candle.close)
         }
-        ic(deep_position)
+        all_positions.append(positions_with_info)
+
+ap = pd.DataFrame(all_positions)
+# ic(ap)
+# ic(ap[['quantity_lot','lot','price','quantity']])
+full_price = ap['quantity'] * ap['price']
+# ic(full_price)
+total = full_price.sum() 
+# ic(total)
+rate = full_price/total
+# ic(rate)
+target_rate = get_target_rate()
+delta_rate = target_rate - rate
+# ic(delta_rate)
+delta_price = delta_rate * full_price
+# ic(delta_price) 
+delta_quantity = delta_price / ap['price']
+# ic(delta_quantity) 
+delta_lot = (delta_quantity / ap['lot']).round()
+delta_lot.name = 'lot_changes'
+# ic(delta_lot)
+result = ap.merge(delta_lot,left_index=True,right_index=True)[['figi','ticker','name','lot_changes']]
+ic(result)
